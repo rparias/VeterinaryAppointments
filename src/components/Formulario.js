a5a1dde2e3c9cfe4993fceb92fb97647bd770d97
Ronald Arias <ronald.arias@thoughtworks.com>
2020-03-03 22:12:23 +0530

    Added useState for manage state in form

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Formulario() {
+  const [cita, actualizarCita] = useState({
+    mascota: '',
+    propietario: '',
+    fecha: '',
+    hora: '',
+    sintomas: ''
+  });
+
+  const handleChange = () => {
+    console.log('escribiendo...');
+  };
+
   return (
     <div>
       <h2>Crear cita</h2>
@@ -12,6 +24,7 @@ function Formulario() {
           id="mascota"
           className="u-full-width"
           placeholder="Nombre Mascota"
+          onChange={handleChange}
         />
         <label htmlFor="propietario">Nombre del Dueño</label>
         <input
@@ -20,16 +33,28 @@ function Formulario() {
           id="propietario"
           className="u-full-width"
           placeholder="Nombre Propietario"
+          onChange={handleChange}
         />
         <label>Fecha</label>
-        <input type="date" name="fecha" className="u-full-width" />
+        <input
+          type="date"
+          name="fecha"
+          className="u-full-width"
+          onChange={handleChange}
+        />
         <label>Hora</label>
-        <input type="time" name="hora" className="u-full-width" />
+        <input
+          type="time"
+          name="hora"
+          className="u-full-width"
+          onChange={handleChange}
+        />
         <label htmlFor="sintomas">Síntomas</label>
         <textarea
           name="sintomas"
           id="sintomas"
           className="u-full-width"
+          onChange={handleChange}
         ></textarea>
         <button type="submit" className="u-full-width button-primary">
           Agregar Cita
